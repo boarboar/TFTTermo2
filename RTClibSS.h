17fb1675845cacc21ca52584cf26b82299e4cb3c
Alex Ershov <alex.ershov@gmail.com>
2015-02-04 10:28:42 +0300

    rtlib year fix

--- RTClibSS.h
+++ RTClibSS.h
@@ -14,7 +14,8 @@ public:
     //DateTime (const char* date, const char* time);
     
     
-    uint16_t year() const       { return 2000 + yOff; }
+    //uint16_t year() const       { return 2000 + yOff; }
+    uint8_t year() const       { return yOff; } // after 2000
     uint8_t month() const       { return m; }
     uint8_t day() const         { return d; }
     uint8_t hour() const        { return hh; }
